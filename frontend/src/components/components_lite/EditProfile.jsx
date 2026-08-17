423e3875300ddf8a2c7eb092b3d7f16203662688
TanuTanu511 <shernitanu2531@gmail.com>
2026-08-17 17:24:40 +0530

    bio edit

--- frontend/src/components/components_lite/EditProfile.jsx
+++ frontend/src/components/components_lite/EditProfile.jsx
@@ -1,28 +1,62 @@
-import React from 'react'
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '../ui/dialog'
-import { Label } from '../ui/label'
+import React from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import { Label } from "../ui/label";
 
-const EditProfile = ({open , setOpen}) => {
+const EditProfile = ({ open, setOpen }) => {
   return (
-    <div className='bg-white'>
-      <Dialog open={open}>
+    <div>
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent>
-            <DialogHeader>
-                <DialogTitle>Edit Profile</DialogTitle>
-            </DialogHeader>
+          <DialogHeader>
+            <DialogTitle>Edit</DialogTitle>
+          </DialogHeader>
 
-            {/* Form for edit Profile */}
-            <form>
-                <div className='grid gap-4 py-4'>
-                    <Label htmlFor="name">Name</Label>
-                    <input type='text' name='name' id='name' className='w-full border border-gray-400 rounded-md p-3'/>
-                </div>
-            </form>
+          {/* Form for edit Profile */}
+          <form>
+            <div className="grid grid-cols-4 items-center gap-4 py-4">
+              <Label htmlFor="name">Name</Label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="col-span-3 border border-gray-400 rounded-md p-3"
+              />
+            </div>
 
+            <div className="grid grid-cols-4 items-center gap-4 py-4">
+              <Label htmlFor="email">Email</Label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="col-span-3 border border-gray-400 rounded-md p-3"
+              />
+            </div>
+
+            <div className="grid grid-cols-4 items-center gap-4 py-4">
+              <Label htmlFor="name">Name</Label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="col-span-3 border border-gray-400 rounded-md p-3"
+              />
+            </div>
+
+            <div className="grid grid-cols-4 items-center gap-4 py-4">
+              <Label htmlFor="name">Name</Label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="col-span-3 border border-gray-400 rounded-md p-3"
+              />
+            </div>
+          </form>
         </DialogContent>
       </Dialog>
     </div>
-  )
-}
+  );
+};
 
-export default EditProfile
+export default EditProfile;
